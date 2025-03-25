b015ca5b598abb23380d7346994a88b489e02683
endohizumi <endohizumi0114@gmail.com>
2025-03-26 02:22:45 +0900

    プロジェクトの初期設定とHighChartsの動作確認を行った

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Highchartsサンプル</h1>
+      <highcharts :options="chartOptions" />
+  </div>
+</template>
+
+<script setup>
+
+const chartOptions = {
+  chart: {
+    type: 'line'
+  },
+  title: {
+    text: 'サンプルチャート'
+  },
+  xAxis: {
+    categories: ['1月', '2月', '3月', '4月', '5月']
+  },
+  yAxis: {
+    title: {
+      text: '値'
+    }
+  },
+  series: [{
+    name: 'データ1',
+    data: [10, 20, 15, 25, 30]
+  }]
+}
+</script>
+
+<style scoped>
+div {
+  padding: 20px;
+}
+</style>
