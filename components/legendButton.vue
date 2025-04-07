ab0d292ea282736a400183b7060c87f888168d4a
EndoHizumi <endohizumi0114@gmail.com>
2025-04-07 15:54:03 +0900

    都道府県選択ボタンのテストを追加した

--- components/legendButton.vue
+++ components/legendButton.vue
@@ -1,14 +1,37 @@
 <template>
-    <div :id="`prefecture-${props.id}`" :style="{ backgroundColor: bgColor }"
+    <div v-if="isValid"
+        :id="`prefecture-${props.id}`"
+        :style="{ backgroundColor: bgColor }"
         class="item w-[67px] h-[67px] bg-gray-200 rounded-xl flex flex-col justify-center items-center select-none cursor-pointer hover:opacity-70"
-        @click="onClick">
+        role="button"
+        tabindex="0"
+        @click="onClick"
+        @keydown.enter="onClick"
+        @keydown.space.prevent="onClick">
         <img :src="prefecturesMap[props.id].icon" :alt="props.name" class="w-[45px] h-[40px]">
         <span>{{ props.name }}</span>
+    </div>
+    <div v-else
+        class="item w-[67px] h-[67px] bg-gray-300 rounded-xl flex flex-col justify-center items-center">
+        <span class="text-red-500">エラー</span>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref, computed } from 'vue';
+import { inject } from 'vue';
 
+interface Prefecture {
+  icon: string;
+  color: string;
+  name: string;
+}
+
+interface PrefecturesMap {
+  [key: number]: Prefecture;
+}
+
+const prefecturesMap = inject<PrefecturesMap>('prefecturesMap', {});
 const props = defineProps({
     id: { type: Number, required: true },
     name: { type: String, required: true },
@@ -16,6 +39,15 @@ const props = defineProps({
 const emit = defineEmits(['click']);
 const bgColor = ref('rgb(229, 231, 235)');
 const isClicked = ref(false);
+
+const isValid = computed(() => {
+    return props.id != null &&
+           props.name != null &&
+           typeof props.id === 'number' &&
+           typeof props.name === 'string' &&
+           props.name !== '' &&
+           prefecturesMap[props.id] != null;
+});
 
 const onClick = () => {
     isClicked.value = !isClicked.value;
