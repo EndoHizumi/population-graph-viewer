709abbf6ef7c4539f6293150bce75fc6439c7239
endohizumi <endohizumi0114@gmail.com>
2025-04-09 00:19:17 +0900

    命名規則などルールを増やした

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 import stylistic from '@stylistic/eslint-plugin'
+import jsdoc from 'eslint-plugin-jsdoc'
+import unicorn from 'eslint-plugin-unicorn'
+import tsParser from '@typescript-eslint/parser'
 
 export default withNuxt(
   // Your custom configs here
@@ -13,12 +16,80 @@ export default withNuxt(
   },
   {
     plugins: {
-      stylistic
+      stylistic,
+      unicorn,
+      jsdoc,
+    },
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+        tsconfigRootDir: '.',
+      },
     },
     rules: {
       'stylistic/semi': ['error', 'never'],
-      'stylistic/max-len': ['error', { 'code': 120 }],
-      'stylistic/quotes': ['error', 'single']
-    }
-  }
+      'stylistic/max-len': ['error', { code: 120 }],
+      'stylistic/quotes': ['error', 'single'],
+      // コーディングスタイルルール
+      'unicorn/catch-error-name': 'warn',
+      eqeqeq: ['error', 'smart'],
+      'vue/multi-word-component-names': 'warn',
+      'vue/no-v-html': 'warn',
+      'vue/require-default-prop': 'error',
+      'vue/require-explicit-emits': 'error',
+      // 命名規則
+      '@typescript-eslint/naming-convention': [
+        'error',
+        { selector: 'variable', format: ['camelCase', 'UPPER_CASE'] },
+        { selector: 'function', format: ['camelCase'] },
+        { selector: 'class', format: ['PascalCase'] },
+        { selector: 'typeAlias', format: ['PascalCase'] },
+        { selector: 'typeProperty', format: ['PascalCase'] },
+        { selector: 'interface', format: ['camelCase'] },
+        {
+          selector: 'variable',
+          types: ['array'],
+          format: ['camelCase'],
+          suffix: ['List'],
+        },
+        {
+          selector: 'variable',
+          modifiers: ['destructured'],
+          format: null,
+          leadingUnderscore: 'allow',
+        },
+      ],
+      // JSDocルール
+      'jsdoc/require-jsdoc': [
+        'error',
+        {
+          require: {
+            ArrowFunctionExpression: true,
+            ClassDeclaration: true,
+            FunctionDeclaration: true,
+            FunctionExpression: true,
+            MethodDefinition: true,
+          },
+        },
+      ],
+      'jsdoc/require-description': [
+        'error',
+        {
+          contexts: [
+            'ArrowFunctionExpression',
+            'ClassDeclaration',
+            'FunctionDeclaration',
+            'FunctionExpression',
+            'MethodDefinition',
+          ],
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', '.nuxt/**', '.output/**','vitest*','*.spec.ts'],
+  },
 )
